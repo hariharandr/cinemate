0172797e2db0f58572439f520aa64108728d4d2e
Hariharan <hariravidhivya@gmail.com>
2024-05-19 15:06:57 +0000

    font awesome icons cdn added

--- htdocs/index.php
+++ htdocs/index.php
@@ -10,7 +10,10 @@ require_once 'src/load.php';
     <title>Cinemate - Your mate to find best Movies, TV Shows, and Celebse</title>
     <link rel="apple-touch-icon" href="./assets/cinimate-logo.webp">
     <link rel="icon" type="image/png" href="./assets/cinimate-logo.webp">
+    <!-- Own Custom CSS -->
     <link rel="stylesheet" href="./css/app.css">
+
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
@@ -26,6 +29,9 @@ require_once 'src/load.php';
     <!-- Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script> <!-- Masonry CDN -->
+    <!-- Fontawesome -->
+    <script src="https://kit.fontawesome.com/7990384401.js" crossorigin="anonymous"></script>
+    <!-- Masonry -->
     <!-- <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script> -->
 </body>
 
